578c47bf33ae5bf507a753a7a30176b426dbefcf
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-03-26 17:03:32 +0200

    added generate script

--- db_generate.py
+++ db_generate.py
@@ -6,6 +6,25 @@ from app.wilaya.models import Wilaya
 from app.commune.models import Commune
 import json
 
+db.create_all()
+
+# creation of all wilayas from wilaya.json
+with open("wilaya.json", "r") as wilaya_json:
+    wilayas = json.load(wilaya_json)
+
+for wilaya in wilayas:
+    db.session.add(Wilaya(wilaya_name=wilaya["wilaya"].lower()))
+    db.session.commit()
+
+# creation of all communes from communes.json
+with open("communes.json", "r") as communes_json:
+    communes = json.load(communes_json)
+
+for commune in communes:
+    wilaya=Wilaya.query.get(int(commune["wilaya_id"]))
+    db.session.add(Commune(name=commune["name"].lower(),wilaya=wilaya))
+    db.session.commit()
+
 #create admin user for instituion initial creation
 
 # creation of insititution classes
